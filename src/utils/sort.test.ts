0ad478e97b65547a2564e545d276bf57b9a95336
msakwins <marion.sakwi@gmail.com>
2022-07-07 15:40:35 +0200

    added few tests

--- src/utils/sort.test.ts
+++ src/utils/sort.test.ts
@@ -1,9 +1,19 @@
-import sortData from '../utils/sort';
+import sort from '../utils/sort';
+
+const goodLetter = [{customer_name: 'aa a'}, {customer_name: 'ba ab'}, {customer_name: 'aa bb'}];
+const badLetter = [{customer_name: 'aa bb'}, {customer_name: 'aa a'}, {customer_name: 'ba ab'}];
+
+const goodFontaine = [{customer_name: 'Jean de Fontaine'}, {customer_name: 'Jean de La Fontaine'}, {customer_name: 'Jean-Marie de La Fontaine Salut'}];
+const badFontaine = [{customer_name: 'Jean-Marie de La Fontaine Salut'}, {customer_name: 'Jean de La Fontaine'}, {customer_name: 'Jean de Fontaine'}];
 
 test('verify sorting basic', () => {
-  expect(sortData([], 'name', 'ASC')).toStrictEqual([]);
+  expect(sort([], 'name', 'ASC')).toStrictEqual([]);
 });
 
 test('verify sorting basic1', () => {
-  expect(sortData([{customer_name: 'aa'}, {customer_name: 'cc'}, {customer_name: 'bb'}], 'name', 'ASC')).toStrictEqual([{customer_name: 'aa'}, {customer_name: 'bb'}, {customer_name: 'cc'}]);
+  expect(sort(badLetter, 'name', 'ASC')).toStrictEqual(goodLetter);
+});
+
+test('verify sorting basic1', () => {
+  expect(sort(badFontaine, 'name', 'ASC')).toStrictEqual(goodFontaine);
 });
--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,6 +1,6 @@
-export default (paymentsData:any, sortingType:string, sortingRule:string) => {
+const sort = (paymentsData:any, sortingType:string, sortingRule:string) => {
   const sortLogic = (a:any, b:any) => {
-    if (sortingType === 'customerName') {
+    if (sortingType === 'name') {
       if (a.customer_name.toLowerCase().split(' ').slice(1).filter((a:any) => a !== 'de' && a !== `d'`) > b.customer_name.toLowerCase().split(' ').slice(1).filter((b:any) => b !== 'de' && b !== `d'`))
         return (sortingRule === 'ASC' ? 1 : -1);
       else
@@ -19,3 +19,5 @@ export default (paymentsData:any, sortingType:string, sortingRule:string) => {
 };
 
 const sortOrder = ['default', 'not_started', 'ready', 'in_progress', 'paid'];
+
+export default sort;
